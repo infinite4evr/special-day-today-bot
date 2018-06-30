aec22589242117f8fe91fccc978cff8900ac62df
Ashu <sudhanshurocks.rai1@gmail.com>
2018-06-30 11:21:05 +0530

    Removed the test code

--- cron.php
+++ cron.php
@@ -12,12 +12,10 @@ $data= getdate();
 $month =  $data['month'];
 $date  =  $data['mday'];
 $hours  = $data['hours'];
-$minute = $date['minutes'];
+$minute = $data['minutes'];
 
 if($minute>=30)
 return;
-
-file_get_contents($website."/sendmessage?chat_id=330959283&text=Cron was called");
 
 $month = ucwords($month);
 
@@ -27,11 +25,13 @@ if($hours!=00)
 $special_days= file_get_contents('Data/specialdays.txt');
 $special_days= explode("\n",$special_days);
 $date=(string)$date;
+$match=0;
 
 foreach( $special_days as $today )
 {
       if(strstr($today,$month) && strstr($today,$date))
        {    
+          $match=1;
           $length=strlen($today);
           $today[$length-1]=" ";
       
@@ -43,6 +43,17 @@ foreach( $special_days as $today )
 			   file_get_contents($website."/sendmessage?chat_id=".$row['chat_id']."&text=Today's special day: $today");
 			}
 	   }
+}
+
+if($match==0)
+{
+$conn = new mysqli($dbhost, $dbusername, $dbpassword,$dbname);
+            $result= $conn->query("select * from `user_details` ");
+			
+	        		while ($row = $result->fetch_assoc())
+			{
+			   file_get_contents($website."/sendmessage?chat_id=".$row['chat_id']."&text=Today's special day: Nothing special to celebrate today");
+			}
 }
 
 ?>
