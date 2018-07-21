79a7378eafa2cb6920d9e12ac53d384aa78253ec
Ashu <sudhanshurocks.rai1@gmail.com>
2018-07-21 16:44:41 +0530

    Updated most of the shit

--- cron.php
+++ cron.php
@@ -1,18 +1,16 @@
 <?php
 
 require('config.php');
-
-
 file_get_contents("https://insulterbot.herokuapp.com/main.php");
-
 date_default_timezone_set('Asia/Kolkata');
 
-$data= getdate();
-
-$month =  $data['month'];
-$date  =  $data['mday'];
+$data   = getdate();
+$month  = $data['month'];
+$date   = $data['mday'];
 $hours  = $data['hours'];
 $minute = $data['minutes'];
+
+file_get_contents($website . "/sendmessage?chat_id=330959283&text=Cron Called");
 
 if($minute>=30)
 return;
@@ -20,40 +18,58 @@ return;
 $month = ucwords($month);
 
 if($hours!=00)
-	return;
+return;
 
-$special_days= file_get_contents('Data/specialdays.txt');
-$special_days= explode("\n",$special_days);
-$date=(string)$date;
-$match=0;
+$special_days = file_get_contents('Data/specialdays.txt');
+$special_days = explode("\n", $special_days);
+$date         = (string) $date;
+$today_date   = $month . " $date";
+$contents     = "Today's special day\n";
 
-foreach( $special_days as $today )
-{
-      if(strstr($today,$month) && strstr($today,$date))
-       {    
-          $match=1;
-          $length=strlen($today);
-          $today[$length-1]=" ";
-      
-            $conn = new mysqli($dbhost, $dbusername, $dbpassword,$dbname);
-            $result= $conn->query("select * from `user_details` ");
-			
-	        		while ($row = $result->fetch_assoc())
-			{
-			   file_get_contents($website."/sendmessage?chat_id=".$row['chat_id']."&text=Today's special day: $today");
-			}
-	   }
+$match = 0;
+
+foreach ($special_days as $today) {
+    
+    
+    if (strstr($today, $today_date)) {
+        $pos = strpos($today, $today_date);
+        
+        if (strlen($date) == 1) {
+            if (isset($today[$pos + strlen($today_date) + 1]))
+                continue;
+            
+        }
+        
+        $match              = 1;
+        $length             = strlen($today);
+        $today[$length - 1] = " ";
+        $contents           = $contents . "\n" . $today;
+    }
 }
 
-if($match==0)
-{
-$conn = new mysqli($dbhost, $dbusername, $dbpassword,$dbname);
-            $result= $conn->query("select * from `user_details` ");
-			
-	        		while ($row = $result->fetch_assoc())
-			{
-			   file_get_contents($website."/sendmessage?chat_id=".$row['chat_id']."&text=Today's special day: Nothing special to celebrate today");
-			}
+if ($match == 0) {
+    
+    $conn   = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+    $result = $conn->query("select * from `user_details` ");
+    
+    while ($row = $result->fetch_assoc()) {
+        file_get_contents($website . "/sendmessage?chat_id=" . $row['chat_id'] . "&text=Today's special day: Nothing special to celebrate today");
+    }
+    
 }
+
+else {
+    $conn   = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+    $result = $conn->query("select * from `user_details` ");
+    
+    while ($row = $result->fetch_assoc()) {
+        $content = array(
+            'chat_id' => $row['chat_id'],
+            'text' => "$contents\nHope you have a great day!"
+        );
+        $telegram->sendMessage($content);
+    }
+}
+
 
 ?>
