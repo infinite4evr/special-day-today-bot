66375a1016cf77e90425e96216b97d7e3ca04a7e
infinite4evr <ggs.sudhanshu@gmail.com>
2020-06-02 09:36:59 +0530

    new

--- update_data.php
+++ update_data.php
@@ -1,42 +1,20 @@
 <?php
 
-$handle = "false";
+$file = file_get_contents('Data/specialdays.txt');
 
-require_once 'simple_html_dom.php';
 
-$website = "https://en.wikipedia.org/wiki/List_of_commemorative_days";
 
-$html = new simple_html_dom();
+echo "<pre>";
+$arr = json_decode(remove_utf8_bom($file), true);
 
-$myfile = "specialdays.txt";
+print_r($arr);
 
-$handle = fopen($myfile, 'a') or die('Cannot open file:  ' . $myfile);
 
-$html->load_file($website);
 
-$element = $html->find("li");
 
-foreach ($element as $specialday) {
-
-    /* if(strpos($html, 'New Year – January 1') !== true);
-
-    { $handle = fopen($myfile, 'a') or die('Cannot open file:  '.$myfile); }
-
-    if($handle!="false");
-
-     */
-
-    // fwrite($handle, $specialday->title);
-
-    var_dump($specialday->find('div.title'));
-
-    // if(strpos($html, 'New Year\'s Eve – December 31') == true);
-
-    /* {
-fclose($handle);
-
-$handle ="false";
-
-} */
-
+function remove_utf8_bom($text)
+{
+    $bom = pack('H*','EFBBBF');
+    $text = preg_replace("/^$bom/", '', $text);
+    return $text;
 }
